6003dd3f9a29e8a61e2014a8ed4d7e6d7cab5183
Haris Horozovic <haris@balkon.ba>
2024-01-12 14:15:58 +0100

    Fix import for link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,14 +1,15 @@
 import {TextProps, useColorScheme} from 'react-native';
 import getStyle from '../../styles';
 import {getStyleForProp} from '../../utils';
+import {Link as NavLink} from '@react-navigation/native';
 
 export default function Link(props: TextProps) {
   const isDarkMode = useColorScheme() === 'dark';
   const style = getStyle(isDarkMode);
 
   return (
-    <Link {...props} style={getStyleForProp(style.link, props.style)}>
+    <NavLink {...props} style={getStyleForProp(style.link, props.style)}>
       {props.children}
-    </Link>
+    </NavLink>
   );
 }
